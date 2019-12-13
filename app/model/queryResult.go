b20ff5153e919163ea2827b0d7e88d1433079afa
Vasily Popov <1005220@mail.ru>
2019-12-14 00:38:43 +0300

    модификация результата запроса

--- app/model/queryResult.go
+++ app/model/queryResult.go
@@ -1,22 +1,52 @@
 package model
 
-// QueryResult ...
+// QueryResult основная модель приложения
 type QueryResult struct {
-	BaseModel
 	Area           int                      `json:"НомерОбласти"`
 	ResultRequest  []map[string]interface{} `json:"РезультатЗапроса"`
 	ErrorExecution bool                     `json:"ОшибкаВыполнения"`
 	EmptyRequest   bool                     `json:"ПустойЗапрос"`
 	ExchangeJobID  string                   `json:"УИД"`
 	JobID          string                   `json:"УИД_Пакета"`
-	Parameter
+	Options
 }
 
-// Parameter ...
-type Parameter struct {
-	TableName        string              // имя таблицы
-	UseSoftRemove    bool                // использовать мягкое удаление
-	SelectFields     []map[string]string // поля отбора
-	CompareAllFields bool                // сравнивать по всем полям
-	MatchFields      []map[string]string // поля сравнения изменений
+// Options описание настроек
+// для обработки результата запроса
+type Options struct {
+	Description      string   `json:"НаименованиеЗадания"`
+	TableName        string   `json:"ИмяТаблицы"`
+	HardRemoval      bool     `json:"ПолноеУдаление"`
+	SelectionFields  []string `json:"ПоляОтбора"`
+	ComparionFields  []string `json:"ПоляСравнения"`
+	CompareAllFields bool     `json:"СравниватьПоВсемПолям"`
+}
+
+// UseHardDelete возвращает признак полного удаления
+func (q *QueryResult) UseHardDelete() bool {
+	return q.HardRemoval
+}
+
+// NeedCompareAllFields возвращает признак того,
+// что необходимо сравнивать все поля
+func (q *QueryResult) NeedCompareAllFields() bool {
+	return q.CompareAllFields
+}
+
+// GetTargetTableName возвращает имя таблицы,
+// над которой будут выполняться действия
+func (q *QueryResult) GetTargetTableName() string {
+	return q.TableName
+}
+
+// GetSelectionFields возвращает поля таблицы,
+// по которым необходимо выполнять отбор
+func (q *QueryResult) GetSelectionFields() []string {
+	return q.SelectionFields
+}
+
+// GetComparisonFields возвращает поля таблицы,
+// по которым необходимо выполнять сравнение
+func (q *QueryResult) GetComparisonFields() []string {
+	return q.ComparionFields
 }
