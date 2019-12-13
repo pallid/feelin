10dcb72e31b18add8a9efdc8a29186aa223b7307
vasily popov <1005220@mail.ru>
2019-12-13 16:00:18 +0300

    add method Close in repositorys interface

--- app/store/repository.go
+++ app/store/repository.go
@@ -4,18 +4,21 @@ import "github.com/pallid/feelin/app/model"
 
 // JobRepository ...
 type JobRepository interface {
+	Close()
 	Update(*model.Job) error
 	Find(string) (*model.Job, error)
 }
 
 // JobSettingRepository ...
 type JobSettingRepository interface {
+	Close()
 	Update(*model.JobSetting) error
 	FindAll(string) (*[]model.JobSetting, error)
 }
 
 // ExchangeJobRepository ...
 type ExchangeJobRepository interface {
+	Close()
 	Update(*model.ExchangeJob) error
 	Find(string, string, string) (*model.ExchangeJob, error)
 }
